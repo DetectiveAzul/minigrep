6ca34286d4aae8bac1f167d16f52ee815b8def9a
Cian <jaime.lopez@qikserve.com>
2023-01-09 12:10:42 +0000

    Creates a constructor for config

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config: Config = parse_config(&args);
+    let config: Config = Config::new(&args);
 
     println!("Searching for string '{}'", config.query);
     println!("In file: '{}'", config.file_path);
@@ -20,9 +20,11 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query: String = args[1].clone();
-    let file_path: String = args[2].clone();
-
-    Config { query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+        
+        Config { query, file_path }
+    }
 }
