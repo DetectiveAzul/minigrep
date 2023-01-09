bd306d39dc0a08f3cd8dcebddac9e0f677ec257f
Cian <jaime.lopez@qikserve.com>
2023-01-09 12:24:07 +0000

    Improves error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config: Config = Config::new(&args);
+    let config: Config = Config::build(&args).unwrap_or_else(|err: &str| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
     println!("Searching for string '{}'", config.query);
     println!("In file: '{}'", config.file_path);
@@ -21,10 +25,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments. You need to specify the string to search for and the path to the file");
+        }
+
         let query: String = args[1].clone();
         let file_path: String = args[2].clone();
         
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
